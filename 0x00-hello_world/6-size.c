c3deaaf1f43fe33a44630a90bcb3a29ce7b71698
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-13 13:47:45 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,12 @@ int main(void)
 char a;
 int b;
 long int c;
-long long d;
- printf("Size of char: %zu byte\n", sizeof(a));
- printf("Size of int: %zu bytes\n", sizeof(b));
- printf("Size of long int: %zu bytes\n", sizeof(c));
- printf("Size of long long int: %zu bytes\n", sizeof(c));
- printf("Size of float: %zu bytes\n", sizeof(d));
-  return (0);
+long long int d;
+float e;
+printf("Size of char: %zu byte\n", sizeof(a));
+printf("Size of int: %zu bytes\n", sizeof(b));
+printf("Size of long int: %zu bytes\n", sizeof(c));
+printf("Size of long long int: %zu bytes\n", sizeof(d));
+printf("Size of float: %zu bytes\n", sizeof(e));
+return (0);
 }

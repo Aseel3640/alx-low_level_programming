81ef90a2bd1e954a3701c66252f3453a550bb85e
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-14 11:38:19 -0700

    c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-char ch = '1';
+char ch = '0';
 char cf = 'a';
-for (ch = '1'; ch <= '9'; ch++)
+for (ch = '0'; ch <= '9'; ch++)
 putchar(ch);
 for (cf = 'a'; cf <= 'f'; cf++)
 putchar(cf);

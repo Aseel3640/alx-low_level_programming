e017b54b041e05da44e3a8a530fbf730d9a7f0ff
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-13 17:03:15 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- *
- * main - Entry point 
- *
- * Return: Always 0 (Success)
+ *main - Entry point 
+ *C program that prints the size of various types
+ *Return: Always 0 (Success)
  */
 int main(void)
 {

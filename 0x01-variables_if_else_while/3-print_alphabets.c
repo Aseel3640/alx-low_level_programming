7ff377133d9ed8f853e0c6a5e18ad3f3e3229120
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-14 09:31:45 -0700

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-/**                                                                                                        
-*
+/**
 *main - Entry point
 *
-*Return:Always 0 (Success)                                                                                               
-*
-*/                                                                                                                        
+*Return:Always 0 (Success)
+*/
 int main(void)
 {
 char ch = 'a';
+char ci = 'A';
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-char ch = 'A';
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
+for (ci = 'A'; ci <= 'Z'; ci++)
+putchar(ci);
 putchar('\n');
 return (0);
-}   
+}

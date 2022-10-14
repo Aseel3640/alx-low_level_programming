ea4cc7a4a7696f197bb2d4aadf193737d4a513bf
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-14 16:43:31 -0700

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,12 @@ int main(void)
 int i;
 for (i = '0'; i <= '9'; i++)
 {
-putchar(((i % 10) + '0');
+putchar((i % 10) + '0');
 if (num == 9)
 continue;
 putchar(',');
 putchar(' ');
+}
 putchar('\n');
 return (0);
 }

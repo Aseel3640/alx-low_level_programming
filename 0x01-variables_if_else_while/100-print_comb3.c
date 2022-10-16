3e5fc49a0975435f1cbddb1803edf19a42b7b3f5
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-16 01:48:07 -0700

    c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,15 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i <= 6;)
-for (j = 1; j <= 9;)
+for (i = 0; i <= 6; i++)
+for (j = 1; j <= 9; j++)
 {
-{
+if( i != j )
 putchar('i, j');
 if (i == j)
 continue;
 putchar(' ');
 putchar(',');
-j++;
-}
-i++;
 }
 putchar('\n');
 return (0);

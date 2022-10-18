293b4d4200ba3518802775581290e77b4aa81326
Aseel3640 <aseelsaifaldi@yahoo.com>
2022-10-18 12:28:37 -0700

    loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,18 +9,33 @@
 int _putchar(char c);
 void print_to_98(int n)
 {
-if (n <= 98)
+if (n < 98)
 {
-printf("%d, ", n);
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
 n++;
 }
-else if (n >= 98)
+}
+else if (n > 98)
 {
-printf("%d, ", n);
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+	printf(", ");
+}
 n--;
+}
 }
 else
 {
 printf("98");
 }
+printf("\n");
 }

8c9a06ea618c858e356cb1266e5bca57c54ddca4
Aseel3640 <aseelsaifaldi@yahoo.com>
2022-10-18 12:52:49 -0700

    loop

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int count = 3;
+long int t1 = 1, t2 = 2, nextTerm;
+printf("Fibonacci Series: %ld, %ld, ", t1, t2);
+nextTerm = t1 + t2;
+while (nextTerm <= 50)
+{
+printf("%ld, ", nextTerm);
+t1 = t2;
+t2 = nextTerm;
+nextTerm = t1 + t2;
+}
+return (0);
+}

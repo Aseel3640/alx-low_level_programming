1fb40192146bfc0c6da33a2f2e3a4fb072e20ba9
Aseel3640 <aseelsaifaldi@yahoo.com>
2022-10-18 12:07:31 -0700

    loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,7 @@
 /**
  *jack_bauer - function that prints every minute of the day, 24 hour clock
  *a = hour, b = minutes
+ *@c:character
  *10 allows second digit to rotate
  *Return: 24 hour clock second in each line
  */

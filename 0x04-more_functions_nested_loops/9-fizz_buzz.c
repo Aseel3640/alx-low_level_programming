36cd3118684398d32c0e8195433ac75a6a8c2fde
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-20 12:19:24 -0700

    loop

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i >= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz\t");
+		}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz\t");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz\t");
+			}
+		else
+		{
+			printf("%d\t", i);
+		}
+	}
+return (0);
+}

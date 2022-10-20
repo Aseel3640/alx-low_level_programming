ca46adb763418c23ff56b8e1f65b292b176302e7
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-20 14:57:04 -0700

    loop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	for (; i <= 9; i++)
+	int i;
+	i = 0;
+	while (i < 10)
 	{
-		if (i == 2 || i == 4)
-		{
-			continue;
-		}
-		else
-		{
+		if (i != 2 || i != 4)
 		_putchar(i + '0');
-		}
+		i++;
 	}
 	_putchar('\n');
 }

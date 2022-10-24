d7c4a28213a2d8b74e74a0f808f22f00539cd12f
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-24 09:08:10 -0700

    pointer

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,18 +6,16 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j = 0;
 
-	while (*(str + i))
+	while (str[j] != '\0')
 	{
-		if (*(str + i) != '\0')
-		{
-			continue;
-		}
-		_putchar(*(str + i));
-		i = i + 2;
-		{
+		j++;
 	}
-		else
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }

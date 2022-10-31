417c3672dd4a73dbe5039a03f5d0e07092d30ae6
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-10-31 07:26:20 -0700

    pointer 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  _strchr -  locates a character in a string
+ *  @s: string
+ *  @c: letter to be found
+ *  Return : letter
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return ('\0');
+}

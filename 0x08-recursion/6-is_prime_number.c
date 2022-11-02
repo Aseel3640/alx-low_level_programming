2e9fd8a10be85f3292131b50a05e7602f1bc330e
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-11-02 09:15:54 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number - find if anumber is prime 
+ * @x: number
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+if ( n % 2 == 0)
+return (0);
+else
+return (1);
+}

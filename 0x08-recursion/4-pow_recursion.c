3f42d18aaab4a76754b8a0113dc90da527efe3bf
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-11-02 07:20:30 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,13 @@
 #include "main.h"
-
 /**
- * _pow_recursion
- * @x: number
- * @y: the power
  *
- * Return: factorial
+ *
+ *
  */
 int _pow_recursion(int x, int y)
 {
-	if (n < 0)
-		return (-1);
-			if (n == 0)
-				return (1);
-			if (x == 0)
-				return (0);
-			return (x * power(x, n - 1));
-}
+	if (y < 0)
+		 return (-1);
+	if (y == 0)
+		return (0);
+	return (x * power(x, y - 1));

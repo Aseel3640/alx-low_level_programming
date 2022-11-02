863b56e34bff786a04e05fe0ba20c1f2fccac347
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-11-02 03:31:55 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-for (; *s != '\0'; s++)
-{
+	if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
+
 	_putchar(*s);
-}
-_putchar('\n');
+	s++;
+	_puts_recursion(s);
 }

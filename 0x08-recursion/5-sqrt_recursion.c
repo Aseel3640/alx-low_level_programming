75e3faeb6e7d56d83df015625919de8bb5534136
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-11-02 08:32:46 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,10 +7,10 @@
  */
 int _sqrt_recursion(int n, int y)
 {
-if (y * y > n)
-return (-1);
-else if (y * y == n)
+if (y * y == n)
 return (y);
+else if (y * y > n)
+return (-1);
 else
 return (_sqrt_recursion(n, y + 1));
 }

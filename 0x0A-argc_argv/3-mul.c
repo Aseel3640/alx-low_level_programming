dd68188ebdcb935b3f2c8944ea7432dd1f762054
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-11-04 02:14:00 -0700

    argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: name
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+return (0);
+}

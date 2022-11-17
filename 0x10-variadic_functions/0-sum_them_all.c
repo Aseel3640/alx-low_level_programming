6e55c26a5d3fffd3200f8cccb2d63e8da77d339e
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-11-17 07:09:25 -0800

    variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of interger
+ * @n: integer to be sum
+ * Return:: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	 va_list ap;
+	 int i, sum;
+
+	 va_start (ap, n);
+
+	 sum = 0;
+	  for (i = 0; i < n; i++)
+		  sum += va_arg (ap, int);
+
+	  va_end (ap);
+	  return sum;
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n:the number of integers passed to the function
+ *
+ * Return:sum of numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+

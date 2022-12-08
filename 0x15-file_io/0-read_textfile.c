cb6a977487f156cbb1117de986a953e473754f92
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-12-08 01:03:07 -0800

    -

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it 
- * to the POSIX standard output
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
  * @filename: name of the file that's read
  * @letters: the number of letters read and printed
- * Returns the actual number of letters it could read and print
+ * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {

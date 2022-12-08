29ee62281e5b86541add2b918e0a8c262e0cbba9
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-12-08 02:05:38 -0800

    -

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,26 @@
-#include "main.h"
+#include"main.h"
+#include <unistd.h>
+#include <fcntl.h>
 /**
- * append_text_to_file - appends text to the end of a file
- * @filename: the name of the file to append text to
- * @text_content: the text to append with NULL termination
+ * create_file - Creates a file with read/write permissions
+ * @filename: name of the file
+ * @text_content: the text to write into the file
  * Return: 1 on success, -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-int fd, wr;
 int len;
+int fd, wr;
 if (filename == NULL)
 return (-1);
 if (text_content == NULL)
 {
+fd = open(filename, O_WRONLY | O_CREAT, 0600);
+if (fd == -1)
+return (-1);
 return (1);
 }
-fd = open(filename, O_WRONLY | O_APPEND);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 for (len = 0; text_content[len]; len++)

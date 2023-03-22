83638c26d0dfb2b35143df0fe104731b294ca455
Aseel3640 <aseelsaifaldin@yahoo.com>
2023-03-22 07:56:58 -0700

    -

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python3
-"""
-5-main
-"""
 
 
 def island_perimeter(grid):
-    """return a island's square"""
-    ln = len(grid[1])
-    llen = len(grid)
-    square = 0
-    for a in range(len(grid)):
-        for b in range(len(grid[a])):
-            if grid[a][b] == 1:
-                if b < ln - 1:
-                    # derecha
-                    if grid[a][b + 1] == 0:
-                        square += 1
-                elif b == ln - 1:
-                    square += 1
-                if b > 0:
-                    # izquierda
-                    if grid[a][b - 1] == 0:
-                        square += 1
-                elif b == 0:
-                    square += 1
-                if a < llen - 1:
-                    # abajo
-                    if grid[a + 1][b] == 0:
-                        square += 1
-                elif a == llen - 1:
-                    square += 1
-                if a > 0:
-                    # arriba
-                    if grid[a - 1][b] == 0:
-                        square += 1
-                elif a == 0 or a == llen:
-                    square += 1
-    return square 
+    """
+    Method island_perimeter:
+    that returns the perimeter of the island described in grid
+    """
+    perim = 0
+    for row in range(len(grid)):
+        for colum in range(len(grid[row])):
+            if grid[row][colum] == 1:
+                if row == 0 or grid[row - 1][colum] == 0:
+                    perim += 1
+                if colum == 0 or grid[row][colum - 1] == 0:
+                    perim += 1
+                if row == len(grid) - 1 or grid[row + 1][colum] == 0:
+                    perim += 1
+                if colum == len(grid[row]) - 1 or grid[row][colum + 1] == 0:
+                    perim += 1
+    return perim
